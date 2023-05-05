2cfa4d37c8609bde6f92e327156f16928f6118dd
NicoleWalkenhorst <119905336+NicoleWalkenhorst@users.noreply.github.com>
2023-05-04 22:20:58 -0600

    images

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+
+$( window ).on( "scroll", function() {
+  $( "span" ).css( "display", "inline" ).fadeOut( "slow" );
+} );
+    });
