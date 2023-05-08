964531b862ffa44b7ca6694f856371260c9169c3
NicoleWalkenhorst <119905336+NicoleWalkenhorst@users.noreply.github.com>
2023-05-08 02:19:52 -0600

    updated jquery

--- js/index.js
+++ js/index.js
@@ -12,10 +12,10 @@ $( window ).on( "scroll", function() {
 ------------------------------------------------------ */
 
 function fade($ele) {
-  $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-      var $next = $(this).next('.quote');
-      fade($next.length > 0 ? $next : $(this).parent().children().first());
- });
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
 }
 fade($('.quoteLoop > .quote').first());
 
@@ -26,27 +26,27 @@ fade($('.quoteLoop > .quote').first());
 
 $(window).scroll(function() {
 
-  if ($(window).scrollTop() > 300) {
-      $('.main_nav').addClass('sticky');
-  } else {
-      $('.main_nav').removeClass('sticky');
-  }
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
 });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
-  if ($('.main_nav').hasClass('open-nav')) {
-      $('.main_nav').removeClass('open-nav');
-  } else {
-      $('.main_nav').addClass('open-nav');
-  }
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
 });
 
 $('.main_nav li a').click(function() {
-  if ($('.main_nav').hasClass('open-nav')) {
-      $('.navigation').removeClass('open-nav');
-      $('.main_nav').removeClass('open-nav');
-  }
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
 });
 
 
@@ -56,19 +56,19 @@ $('.main_nav li a').click(function() {
 
 jQuery(document).ready(function($) {
 
- $('.smoothscroll').on('click',function (e) {
-    e.preventDefault();
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
 
-    var target = this.hash,
-    $target = $(target);
+	    var target = this.hash,
+	    $target = $(target);
 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 800, 'swing', function () {
-        window.location.hash = target;
-    });
-});
-
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
 });
 
 
